38eb6f7295b7c670e0341c47283507f31ea022f5
Raulyoyo <raul.dmai@gmail.com>
2022-04-01 16:50:53 -0600

    TT historia

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+
+import { MDBSpinningPreloader, MDBBootstrapModulesPro, ToastModule } from 'ng-uikit-pro-standard';
+import { MdbCalendarModule } from 'mdb-calendar';
+import { MdbColorPickerModule } from 'mdb-color-picker';
+import { MdbDraggableModule } from 'mdb-draggable';
+import { MdbFileUploadModule } from 'mdb-file-upload';
+import { MdbSortableModule } from 'mdb-sortable';
+import { MdbTableEditorModule } from 'mdb-table-editor';
+import { MdbWysiwygModule } from 'mdb-wysiwyg';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ToastModule.forRoot(),
+    MDBBootstrapModulesPro.forRoot(),
+    MdbCalendarModule,
+    MdbColorPickerModule,
+    MdbDraggableModule,
+    MdbFileUploadModule,
+    MdbSortableModule,
+    MdbTableEditorModule,
+    MdbWysiwygModule,
+  ],
+  providers: [MDBSpinningPreloader, {provide: LocationStrategy, useClass: HashLocationStrategy}],
+  bootstrap: [AppComponent],
+  schemas:      [ NO_ERRORS_SCHEMA ]
+})
+export class AppModule { }
